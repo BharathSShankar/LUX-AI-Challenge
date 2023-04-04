5e4e4962da411ce49e1b6de0fb81756722e36eee
Poto <enversionkiller@gmail.com>
2023-04-04 17:25:54 +0800

    Updated bidding and placement policy

--- agent.py
+++ agent.py
@@ -10,7 +10,8 @@ class Agent():
         np.random.seed(0)
         self.env_cfg: EnvConfig = env_cfg
 
-    def bid_policy(self, obs, w1=5, w2=5, w3=0.1):
+    # TODO: Adjust Weight properly
+    def bid_policy(self, obs, w1=5, w2=5, w3=0.1) -> int:
         ice_map = obs["board"]["ice"]
         ice_tile_locations = np.argwhere(ice_map == 1)
         ore_map = obs["board"]["ore"]
@@ -40,12 +41,87 @@ class Agent():
         elif smallest_distance > 36:
             w2 = 0
 
-        # TODO: Add rubble area as a bidding factor
-
         bid = w1 * res_count + w2 * smallest_distance
         return bid
+    
+    # TODO: Adjust Weight Properly
+    def placement_policy(self, potential_spawns: np.ndarray, gameState: GameState):
 
-    # TODO: Placement Policy
+        closestIceWeight = 0.5
+        closestOreWeight = 0.1
+        iceWeight = 0.05
+        oreWeight = 0.01
+        rubbleWeight = 0.001
+        enemyFactoryWeight = -0.01
+
+        # Distance to Ice Res Tiles
+        ice_map = gameState.board.ice
+        ice_tile_locations = np.argwhere(ice_map == 1)
+        ice_tile_distances = np.full((EnvConfig.map_size, EnvConfig.map_size), np.inf)
+
+        # Distance to Ore Res Tiles
+        ore_map = gameState.board.ore
+        ore_tile_locations = np.argwhere(ore_map == 1)
+        ore_tile_distances = np.full((EnvConfig.map_size, EnvConfig.map_size), np.inf)
+
+        # Rubble Count 
+        rubble_map = gameState.board.rubble
+        rubble_count = np.full((EnvConfig.map_size, EnvConfig.map_size), np.inf)
+
+        # Enemy Factory
+        enemy_factory = gameState.factories["player_0" if self.player == "player_1" else "player_1"]
+        enemy_factory_distances = np.zeros((EnvConfig.map_size, EnvConfig.map_size))
+
+        for i in range(len(EnvConfig.map_size)):
+            for j in range(len(EnvConfig.map_size)):
+                if potential_spawns[i][j] == True:
+                    # General Res Distances
+                    ice_tile_distances[i, j] = iceWeight*np.mean((ice_tile_locations-[i,j])**2, 1)
+                    ore_tile_distances[i, j] = oreWeight*np.mean((ore_tile_locations-[i,j])**2, 1)
+                    
+                    # Distance to Closest Ice Tile
+                    closest_ice_tile = ice_tile_locations[np.argmin(ice_tile_distances[i, j])]
+                    ice_tile_distances[i, j] += closestIceWeight*(np.linalg.norm(closest_ice_tile-[i, j]))
+
+                    # Distance to Closest Ore Tile
+                    closest_ore_tile = ore_tile_locations[np.argmin(ore_tile_distances[i, j])]
+                    ore_tile_distances[i, j] += closestOreWeight*(np.linalg.norm(closest_ore_tile-[i, j]))
+
+                    # Rubble Mean Low Count (7, 7) window size - (3, 3) factory size
+                    rubble_tile_count = 0
+                    total_rubble_count = 0
+                    for window_row in range(i-3,i+4): # (7, 7) window size
+                        for window_col in range(j-3, j+4):
+                            if window_row < 0 or window_row > EnvConfig.map_size or window_col < 0 or window_col > EnvConfig.map_size:
+                                continue
+                            else:
+                                rubble_tile_count+=1
+                                total_rubble_count += rubble_map[window_row][window_col]
+                    for window_row in range(i-1, i+2): # (3, 3) factory size
+                        for window_col in range(j-1, j+2):
+                            if window_row < 0 or window_row > EnvConfig.map_size or window_col < 0 or window_col > EnvConfig.map_size:
+                                continue
+                            else:
+                                rubble_tile_count-=1
+                                total_rubble_count -= rubble_map[window_row][window_col]
+                    rubble_count[i][j] = rubbleWeight * (total_rubble_count/rubble_tile_count)
+
+                    # Enemy Factory Distance
+                    if len(enemy_factory) > 0:
+                        for unit_id, factory in enemy_factory.items():
+                            enemy_factory_distances[i][j] += (enemyFactoryWeight*np.linalg.norm(factory.pos - [i, j]))
+
+        combine_value = np.add(ice_tile_distances, ore_tile_distances, rubble_count, enemy_factory_distances)
+
+        water_left = gameState.teams[self.player].water
+        metal_left = gameState.teams[self.player].metal
+        spawn_loc = potential_spawns[np.argmin(combine_value)]
+        if water_left > 150 and metal_left > 150:
+            metal_amount,water_amount = 150, 150
+        else:
+            metal_amount,water_amount = water_left, metal_left        
+        return spawn_loc, metal_amount, water_amount
+
     def early_setup(self, step: int, obs, remainingOverageTime: int = 60):
         if step == 0:
             # bid 0 to not waste resources bidding and declare as the default faction
@@ -66,8 +142,8 @@ class Agent():
             if factories_to_place > 0 and my_turn_to_place:
                 # we will spawn our factory in a random location with 150 metal and water if it is our turn to place
                 potential_spawns = np.array(list(zip(*np.where(obs["board"]["valid_spawns_mask"] == 1))))
-                spawn_loc = potential_spawns[np.random.randint(0, len(potential_spawns))]
-                return dict(spawn=spawn_loc, metal=150, water=150)
+                spawn_loc, metal_amount, water_amount = Agent.placement_policy(potential_spawns, game_state)
+                return dict(spawn=spawn_loc, metal=metal_amount, water=water_amount)
             return dict()
 
     def act(self, step: int, obs, remainingOverageTime: int = 60):
